4524f1e77e8465380454d8de4bdc3efc9d434bd9
LUKY X RXVVX <aseppak793@gmail.com>
2026-02-26 07:08:44 +0700

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  botToken: process.env.BOT_TOKEN,
+  ownerId: process.env.OWNER_ID,
+  prefix: ".",
+  botName: "BOT DUEL RXVVX",
+  ownerName: "Irsan",
+  depositNumber: "083173495612"
+};
