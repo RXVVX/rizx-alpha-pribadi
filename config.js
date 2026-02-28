2b53fbd3742af6a4eb7f2c5408218cc875a2d4ea
LUKY X RXVVX <aseppak793@gmail.com>
2026-03-01 02:43:01 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  botToken: process.env.BOT_TOKEN,
-  ownerId: process.env.OWNER_ID,
-  prefix: ".",
-  botName: "BOT DUEL RXVVX",
-  ownerName: "Irsan",
-  depositNumber: "083173495612"
-};
